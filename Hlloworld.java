d7c403046585bf583c05314aa1633f5476b68127
neojarvis <techadmin@iamneo.ai>
2025-04-22 11:41:43 +0000

    Manual-save-commit-22/04/2025--17:11:41

---
+++ Hlloworld.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Hlloworld {
+    Scanner scanner = new Scanner(system.in)
+    System.out.print("Enter Your Name: ")
+    String name = scanner.nextLine();
+}
